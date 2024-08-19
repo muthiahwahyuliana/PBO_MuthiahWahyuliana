ce10e54591d064634e026a02c6b605e422c44351
Muthiah Wahyuliana <166980141+muthiahwahyuliana@users.noreply.github.com>
2024-08-19 13:19:49 +0700

    Add files via upload
    Tugas Praktkum 2

---
+++ TugasPBO2.php
@@ -0,0 +1,19 @@
+<?php
+ $c = 20;
+ $R = 10;
+class TemperatureConverter
+{
+    // Konversi dari Celsius ke Kelvin
+    var_dump ($c + 273);
+    echo "<br>";
+
+    // Konversi dari Celsius ke Farenheit
+    var_dump ($c + 32);
+    echo "<br>";
+
+    // Konversi dari Celsius ke Reamur
+    var_dump ($R/0.8);
+    echo "<br>";
+   
+}
+?>
